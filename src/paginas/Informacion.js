11408a153548de17aed9b82ce1f528ea5d993c8e
Pamela Encinas <pamelaencinas23@gmail.com>
2024-12-16 14:58:42 -0400

    ultimo

--- src/paginas/Informacion.js
+++ src/paginas/Informacion.js
@@ -25,15 +25,15 @@ const Informacion = () => {
     <div className="carousel-item">
       <img src={mueble2} className="d-block w-100  tam" alt="..."/>
       <div className="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
+        <h5>MI PROYECTO</h5>
+        <p>PAMELA INGRID ENINAS REJAS</p>
       </div>
     </div>
     <div className="carousel-item">
       <img src={mueble3} className="d-block w-100 tam" alt="..."/>
       <div className="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
+        <h5>MI PROYECTO</h5>
+        <p>PAMELA INGRID ENINAS REJAS</p>
       </div>
     </div>
   </div>
